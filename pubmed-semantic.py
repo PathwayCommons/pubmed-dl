42389a41e77fa12adae96fd2b47036bb50ec85d5
Anweshi Anavadya <57552053+Anwesh1@users.noreply.github.com>
2021-01-28 11:19:08 -0500

    Update pubmed-semantic.py

--- pubmed-semantic.py
+++ pubmed-semantic.py
@@ -1,16 +1,18 @@
 import json
 import requests
 import os
+import sys
 from ncbi import uids_to_docs , get_list_pmid
 
 def main():
     ab = open("test.json", 'w')
-    store = get_list_pmid("2020/12/01","2020/12/02")
-    print(len(store))
-    here = uids_to_docs(store)
+    start = input("Enter start date (YYYY/MM/DD): ")
+    end = input("Enter end date (YYYY/MM/DD): ")
+    here = uids_to_docs(get_list_pmid(start,end))
     for item in here:
         ab.write(json.dumps(item))
         ab.write("\n")
+    print("Done writing data from "+start+" to "+end+" onto file named: test.json")
     ab.close()
 
 if __name__ == "__main__":
