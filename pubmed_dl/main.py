e987981d44bde2d5b41a335e82a2097b86c3bb0f
Anweshi Anavadya <57552053+Anwesh1@users.noreply.github.com>
2021-02-02 16:23:42 -0500

    Update main.py

--- pubmed_dl/main.py
+++ pubmed_dl/main.py
@@ -1,8 +1,7 @@
+#!/usr/bin/env python
 import json
-import requests
-import os
-import sys
-from ncbi import uids_to_docs , get_list_pmid
+
+from pubmed_dl.ncbi import get_list_pmid, 
 
 def main():
     ab = open("test.json", 'w')
