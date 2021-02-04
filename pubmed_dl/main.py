c3de702522f7b7e96ca722bd50d7ec4c7ed28f41
Anweshi Anavadya <57552053+Anwesh1@users.noreply.github.com>
2021-02-04 18:38:19 -0500

    Update main.py

--- pubmed_dl/main.py
+++ pubmed_dl/main.py
@@ -7,10 +7,11 @@ def main():
     start = input("Enter start date (YYYY/MM/DD): ")
     end = input("Enter end date (YYYY/MM/DD): ")
     val = get_list_pmid(start,end)
-    for batch in uids_to_docs(val):
-        ab.write("\n".join([json.dumps(doc) for doc in batch]))  
+    here = uids_to_docs(val)
+    for item in here:
+        ab.write(json.dumps(item))
+        ab.write("\n")
     print(f"Done writing data from {start} to {end} onto file named: test.json")
     ab.close()
-
 if __name__ == "__main__":
     main()
