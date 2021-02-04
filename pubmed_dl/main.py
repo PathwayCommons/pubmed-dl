1e40bab1d1d9ecac46c0afbeb27a2b8cfe6afce1
Anweshi Anavadya <57552053+Anwesh1@users.noreply.github.com>
2021-02-04 12:28:11 -0500

    Add files via upload

--- pubmed_dl/main.py
+++ pubmed_dl/main.py
@@ -1,16 +1,21 @@
-#!/usr/bin/env python
 import json
-from pubmed_dl.ncbi import get_list_pmid, uids_to_docs
+import requests
+import os
+import sys
+from ncbi import uids_to_docs , get_list_pmid
 
 def main():
     ab = open("test.json", 'w')
-    start = input("Enter start date (YYYY/MM/DD): ")
-    end = input("Enter end date (YYYY/MM/DD): ")
+    #start = input("Enter start date (YYYY/MM/DD): ")
+    #end = input("Enter end date (YYYY/MM/DD): ")
+    start = "2020/12/04"
+    end = "2020/12/07"
     val = get_list_pmid(start,end)
+    val[-1] = "bruh"
     here = uids_to_docs(val)
-    for item in here:
-        ab.write(json.dumps(item))
-        ab.write("\n")
+    for item in uids_to_docs(val):
+        ab.write(json.dumps(item) + '\n')
+
     print(f"Done writing data from {start} to {end} onto file named: test.json")
     ab.close()
 
