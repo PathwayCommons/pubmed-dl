2c9ab0ae49a6bc7a1119af2887104ffef23cd85a
Anweshi Anavadya <57552053+Anwesh1@users.noreply.github.com>
2021-02-05 15:30:26 -0500

    Update main.py

--- pubmed_dl/main.py
+++ pubmed_dl/main.py
@@ -4,13 +4,13 @@ from pubmed_dl.ncbi import get_list_pmid, uids_to_docs
 
 def main(start: str, end: str):
     start_date = start
-    end = end
-    val = get_list_pmid(start_date,end)
+    end_date = end
+    val = get_list_pmid(start_date,end_date)
     with open("test.json", "w") as f:
         for batch in uids_to_docs(val):
             for doc in batch:
                 f.write(f"{json.dumps(doc)}\n")
-    print(f"Done writing data from {start_date} to {end} onto file named: test.json")
+    print(f"Done writing data from {start_date} to {end_date} onto file named: test.json")
 
 if __name__ == "__main__":
     typer.run(main)
